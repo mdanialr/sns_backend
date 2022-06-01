0a09c8adbe07524aefc65e8050f2be0de275ec49
mdanialr <mdanialrma@gmail.com>
2022-06-01 20:13:38 +0700

    add test for middleware that check whether ID Shorten is exists in database

--- internal/middleware/shorten_validation_test.go
+++ internal/middleware/shorten_validation_test.go
@@ -2,11 +2,16 @@ package middleware
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang/mock/gomock"
+	mockdb "github.com/mdanialr/sns_backend/internal/database/mock"
+	database "github.com/mdanialr/sns_backend/internal/database/sql"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -70,6 +75,70 @@ func TestCreateShortenValidation(t *testing.T) {
 			res, _ := app.Test(req)
 
 			assert.Equal(t, tc.expectStatusCode, res.StatusCode)
+			var r JsonResponse
+			_ = json.NewDecoder(res.Body).Decode(&r)
+			assert.Contains(t, r.Msg, tc.expectMsg)
+		})
+	}
+}
+
+func TestIsIdExistsValidation(t *testing.T) {
+	testCases := []struct {
+		name       string
+		sample     int64
+		buildStubs func(sns *mockdb.MockSNS, id int64)
+		expectCode int
+		expectMsg  string
+	}{
+		{
+			name: "Should fail when database failed to get the intended shorten data, this error probably caused by" +
+				" the ID is not exist in database therefor return 404 code",
+			sample: 2,
+			buildStubs: func(s *mockdb.MockSNS, id int64) {
+				s.EXPECT().
+					GetShorten(gomock.Any(), id).
+					Times(1).
+					Return(database.Shorten{ID: 0}, sql.ErrNoRows)
+			},
+			expectCode: fiber.StatusNotFound,
+			expectMsg:  "is not found",
+		},
+		{
+			name: "Should pass when using ID that indeed exists in the database and should pass this middleware and" +
+				" get json response from the next handler",
+			sample: 12,
+			buildStubs: func(s *mockdb.MockSNS, id int64) {
+				s.EXPECT().
+					GetShorten(gomock.Any(), id).
+					Times(1).
+					Return(database.Shorten{ID: 12}, nil)
+			},
+			expectCode: fiber.StatusOK,
+			expectMsg:  "success",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			sns := mockdb.NewMockSNS(ctrl)
+			tc.buildStubs(sns, tc.sample)
+
+			app := fiber.New()
+			app.Patch("/:id",
+				IsIdExistsValidation(sns),
+				func(c *fiber.Ctx) error {
+					c.Status(fiber.StatusOK)
+					return c.JSON(fiber.Map{"message": "success"})
+				},
+			)
+
+			req := httptest.NewRequest(fiber.MethodPatch, fmt.Sprintf("/%d", tc.sample), nil)
+			res, _ := app.Test(req)
+
+			assert.Equal(t, tc.expectCode, res.StatusCode)
 			var r JsonResponse
 			_ = json.NewDecoder(res.Body).Decode(&r)
 			assert.Contains(t, r.Msg, tc.expectMsg)
