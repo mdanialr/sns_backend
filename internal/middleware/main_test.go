5fbfa96ed2798113fab0fde23c06f1357ab69fe3
mdanialr <mdanialrma@gmail.com>
2022-06-04 21:48:23 +0700

    add preparation to test Send validation middleware

--- internal/middleware/main_test.go
+++ internal/middleware/main_test.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"log"
 	"os"
 	"testing"
 )
@@ -10,6 +11,14 @@ type JsonResponse struct {
 	Msg string `json:"message"`
 }
 
+const fakeFilePath = "/tmp/fake-test-file.txt"
+
 func TestMain(m *testing.M) {
+	// prepare fake file for Send test
+	err := os.WriteFile(fakeFilePath, []byte(`hello universe`), 0664)
+	if err != nil {
+		log.Fatalln("failed preparing fake file for testing Send middleware:", err)
+	}
+
 	os.Exit(m.Run())
 }
