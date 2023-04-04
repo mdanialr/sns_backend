78b4d79bef58bcc0219101708ffb9f034c198f34
mdanialr <mdanialrma@gmail.com>
2023-04-04 22:26:10 +0700

    chore: remove unnecessary json tag in domain

--- internal/domain/registered_otp.go
+++ internal/domain/registered_otp.go
@@ -8,11 +8,11 @@ import (
 
 // RegisteredOTP object for table `registered_otp`.
 type RegisteredOTP struct {
-	ID        uint           `json:"id,omitempty" gorm:"primaryKey"`
-	Code      string         `json:"code,omitempty"`
-	CreatedAt *time.Time     `json:"created_at,omitempty"`
-	UpdatedAt *time.Time     `json:"updated_at,omitempty"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	ID        uint `gorm:"primaryKey"`
+	Code      string
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
 func (o *RegisteredOTP) TableName() string {
--- internal/domain/sns.go
+++ internal/domain/sns.go
@@ -8,15 +8,15 @@ import (
 
 // SNS object for table `sns`.
 type SNS struct {
-	ID          uint           `json:"id,omitempty" gorm:"primaryKey"`
-	Url         string         `json:"url,omitempty"`
-	Shorten     *string        `json:"shorten,omitempty"`
-	Send        *string        `json:"send,omitempty"`
-	FileSize    *string        `json:"file_size,omitempty"`
-	IsPermanent *bool          `json:"is_permanent,omitempty"`
-	CreatedAt   *time.Time     `json:"created_at,omitempty"`
-	UpdatedAt   *time.Time     `json:"updated_at,omitempty"`
-	DeletedAt   gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	ID          uint `gorm:"primaryKey"`
+	Url         string
+	Shorten     *string
+	Send        *string
+	FileSize    *string
+	IsPermanent *bool
+	CreatedAt   *time.Time
+	UpdatedAt   *time.Time
+	DeletedAt   gorm.DeletedAt ` gorm:"index"`
 }
 
 func (s *SNS) TableName() string {
